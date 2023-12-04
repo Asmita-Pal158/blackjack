98e5a2b3cf42565daa11040053b06f197239aa1a
Asmita-Pal158 <asmita.pal851@gmail.com>
2023-12-04 10:19:49 +0530

    card adding for user and dealer
    functions with addaing cards for dealer and user estimating the sum both the parties

--- main.py
+++ main.py
@@ -1,12 +1,74 @@
+# Our Blackjack House Rules #####################
 
-############### Our Blackjack House Rules #####################
+# The deck is unlimited in size. # There are no jokers.
+# The Jack/Queen/King all count as 10.
+# The Ace can count as 11 or 1.
+# Use the following list as the deck of cards:y
 
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# The cards in the list have equal probability of being drawn.
+# Cards are not removed from the deck as they are drawn.
+import random
+import art
 
+
+def card_nos(a):
+    for i in range(1, 3):
+        n = random.randint(0, 11)
+        if n == 0:
+            if sum(a) <= 10:
+                n = 1
+            else:
+                n = 11
+            a.append(n)
+        else:
+            a.append(n)
+    return a
+def card_add(a):
+ n = random.randint(0, 11)
+ if n == 0:
+     if sum(a) <= 10:
+         n = 1
+     else:
+        n = 11
+        a.append(n)
+ else:
+        a.append(n)
+ return a
+def dealer_card(a):
+    c = random.randint(0,1)
+    while c == 1:
+        n = random.randint(0, 11)
+        if n == 0:
+         if sum(a) <= 10:
+          n = 1
+         else:
+          n = 11
+          a.append(n)
+        else:
+          a.append(n)
+        if sum(a)>21:
+            break
+    return a   
+ 
+        
+        
+play = input("Do you want to play blackjack, if yes then press y else press n")
+if play == "y":
+    print(art.logo)
+    user = []
+    dealer = []
+    card_nos(user)
+    card_nos(dealer)
+    print("Your cards:", user, "current score:", sum(user))
+    print("Your cards:", dealer, "current score:", sum(dealer))
+    card= input("Type 'y' to get another card, type 'n' to pass:")
+    while card=="y":
+        user= card_add(user)
+        print("Users current hand:",user,"Current Total",sum(user))
+        card= input("Type 'y' to get another card, type 'n' to pass:")
+    else:
+        print("Your final hand:",a,"Your Final score :",sum(a))
+        
+    dealer=dealer_card(dealer) 
+    card_compare(user,dealer)   
